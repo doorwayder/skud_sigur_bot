2bad22ba41275c4ab81cacaab327e1921f0f647f
doorwayder <doorwayder@gmail.com>
2020-09-21 16:58:33 +0300

    TELEGRAM BOT Enabled

---
+++ bot.py
@@ -0,0 +1,22 @@
+from sigur import *
+import telebot
+
+bot = telebot.TeleBot('1397767064:AAEM9LElXOpDt9R9iLniEpdoRkEEmYHzBEM')
+@bot.message_handler(content_types=['text'])
+def start_message(message):
+    if message.text.isdigit():
+        person = Person(tab_no=int(message.text))
+        person.init_data()
+    else:
+        person = Person()
+        person.search_init(message.text)
+
+    if person.initialized:
+        bot.send_photo(183992731, person.get_img_info())
+    else:
+        bot.send_message(183992731, 'Person is not initialized')
+
+bot.polling()
+
+#bot.send_message(183992731, 'Hello')
+
