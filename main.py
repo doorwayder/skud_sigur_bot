89408411c119231044eb02f41d7d125390bd1ca5
doorwayder <doorwayder@gmail.com>
2020-09-21 11:59:15 +0300

    DB Photo functioanlity

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from sigur import *
 
-pvm = Person()
-pvm.init_person()
+moa = Person(tab_no=1059)
+if moa.init_data():
+    print(moa.name, moa.id, moa.tab)
+    print(moa.person_zone_name)
+    moa.get_img_info()
+
+
--- sigur.py
+++ sigur.py
@@ -1,44 +1,43 @@
 import pymysql.cursors
-import time
+from PIL import Image, ImageDraw, ImageFont
+import io
 
 class Person:
 
-    def __init__(self, tab_param=None, id_param=None, name_param=None):
-        if tab_param is not None:
-            self.tab = tab_param
+    def __init__(self, tab_no=None, id_no=None, person_name=None):
+        if tab_no is not None:
+            self.tab = tab_no
         else:
             self.tab = None
-        if id_param is not None:
-            self.id = id_param
+        if id_no is not None:
+            self.id = id_no
         else:
             self.id = None
-        if name_param is not None:
-            self.name = name_param
+        if person_name is not None:
+            self.name = person_name
         else:
             self.name = None
         self.hostname = 'skudsrv'
+        self.initialized = False
 
     # def __del__(self):
     #     # TODO ...
 
     @property
     def person_zone(self):
-        if self.id is None:
-            if self.tab is None:
-                print('Id or Tab is not initialized')
-                return False
-            else:
-                self.id = self.person_id_by_tab
-
-        connection = pymysql.connect(host=self.hostname, port=3305, user='root', password='spnx32_0',
-                                         db='tc-db-main', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
-        try:
-             with connection.cursor() as cursor:
-                sql = "SELECT NAME, LOCATIONZONE FROM Personal WHERE Id = {}".format(self.id)
-                cursor.execute(sql)
-        finally:
-            connection.close()
-        return cursor.fetchone()['LOCATIONZONE']
+        if self.initialized:
+            connection = pymysql.connect(host=self.hostname, port=3305, user='root', password='spnx32_0',
+                                             db='tc-db-main', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+            try:
+                 with connection.cursor() as cursor:
+                    sql = "SELECT NAME, LOCATIONZONE FROM Personal WHERE Id = {}".format(self.id)
+                    cursor.execute(sql)
+            finally:
+                connection.close()
+            return cursor.fetchone()['LOCATIONZONE']
+        else:
+            print('Person is not initialized')
+            return False
 
     @property
     def person_id_by_tab(self):
@@ -55,12 +54,15 @@ class Person:
 
     @property
     def person_zone_name(self):
-        if self.person_zone == 1:
-            return 'внутренняя территория'
+        if self.initialized:
+            if self.person_zone == 1:
+                return 'внутренняя территория'
+            else:
+                return 'внешняя территория'
         else:
-            return 'внешняя территория'
+            return False
 
-    def init_person(self):
+    def init_data(self):
         if not (self.id or self.tab or self.name):
             print('Parameters Id, Tab or Name not defined')
             return False
@@ -76,20 +78,51 @@ class Person:
                 else:
                     if self.name is not None:
                         sql = "SELECT Id, TABID, NAME FROM Personal WHERE NAME = {}".format(self.name)
-
-
-
-
             try:
                 with connection.cursor() as cursor:
-
                     cursor.execute(sql)
+                    result = cursor.fetchall()
+            finally:
+                connection.close()
+            if result:
+                self.id = result[0]['Id']
+                self.tab = int(result[0]['TABID'])
+                self.name = result[0]['NAME']
+                self.initialized = True
+                return True
+            else:
+                self.initialized = False
+                print('Person not defined')
+                return False
 
+    def get_person_photo(self):
+        if self.initialized:
+            connection = pymysql.connect(host=self.hostname, port=3305, user='root', password='spnx32_0',
+                                         db='tc-db-main', charset='utf8', cursorclass=pymysql.cursors.DictCursor)
+            try:
+                with connection.cursor() as cursor:
+                    sql = "SELECT HIRES_RASTER FROM photo WHERE ID = {}".format(self.id)
+                    cursor.execute(sql)
 
             finally:
                 # Закрыть соединение (Close connection).
                 connection.close()
-            return cursor.fetchone()['NAME']
+            return cursor.fetchone()['HIRES_RASTER']
+        else:
+            print('Person is not initialized')
+            return False
 
+    def get_img_info(self):
+        if self.initialized:
+            photo = Image.open(io.BytesIO(self.get_person_photo()))
+            draw_text = ImageDraw.Draw(photo)
+            font = ImageFont.truetype('c:/Windows/Fonts/arial.ttf ', size=16)
+            draw_text.text((5, 5), self.name, font=font, fill=('#1C0606'))
+            draw_text.text((5, 20), str(self.tab), font=font, fill=('#1C0606'))
+            draw_text.text((5, 35), self.person_zone_name, font=font, fill=('#1C0606'))
+            photo.show()
+        else:
+            print('Person is not initialized')
+            return False
 
 # TODO Методы получения фото, полной информации о Person, статистику
